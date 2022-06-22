660de19aac958f166e9c58e343e7288525f7423a
mohamedrashad123 <mohamedrashad@dipdux.com>
2022-06-22 08:32:52 +0200

    upload files

--- src/constant/const.js
+++ src/constant/const.js
@@ -1,3 +1,4 @@
-export const baseURL = 'http://localhost:3000';
+// export const baseURL = 'http://localhost:3000';
 // export const cdnLink = 'http://127.0.0.1:5050';
-export const cdnLink = 'https://cdn-ennglish4kids.dipdux.com';
+export const baseURL = 'https://english4kidsapi.dipdux.com';
+export const cdnLink = 'https://cdn-english4kids.dipdux.com';
